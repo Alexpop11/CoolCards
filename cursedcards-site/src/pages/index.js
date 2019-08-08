4194aae55d87d78eac31eea72247d546cad39700
Alexpop11 <alexpopovitch@gmail.com>
2019-08-07 21:13:18 -0400

    Background and fixes

--- cursedcards-site/src/pages/index.js
+++ cursedcards-site/src/pages/index.js
@@ -9,9 +9,17 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-        < div class="container"><h1>The best cards on the net.</h1>
-    <p>lets sell some cards!</p>
-    <p>Now go build something great.</p></div>
+    <div class="bigbox">
+      <div class="container">
+        <h1>The best cards on the net.</h1>
+      </div>
+      <div class="container">
+        <p>lets sell some cards!</p>
+      </div>
+      <div class="container">
+        <p>Now go build something great.</p>
+      </div>
+    </div>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <img src="/hmmmmmmmm.jpg"/>
     </div>
