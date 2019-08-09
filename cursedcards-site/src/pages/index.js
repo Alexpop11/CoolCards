3af06b0bab068828b8e90bc44658b2526b90028e
Alexpop11 <alexpopovitch@gmail.com>
2019-08-08 22:20:41 -0400

    Set up cards, Cardinfo.json

--- cursedcards-site/src/pages/index.js
+++ cursedcards-site/src/pages/index.js
@@ -6,6 +6,19 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+const Card = (props) => (
+  <div>
+    <img className="cardpic" src={props.pic} 
+         onMouseOver={e => (e.currentTarget.src = props.insidepic)}
+         onMouseOut ={e => (e.currentTarget.src = props.pic)}/>
+    <div className="card">
+      <p>{props.name}</p>
+    </div>
+  </div>
+
+)
+
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
@@ -22,6 +35,17 @@ const IndexPage = () => (
         <p>There will be soon, though.</p>
       </div>
     </div>
+
+    <div className="cardgrid">
+       <Card pic="/Minion-link/Minionlink-front.png" insidepic="/Minion-link/Minionlink-inside-preview.png" name="minimalist"/>
+       <Card pic="/Obama-Yoda/Obama-Yoda-Preview.png"  insidepic="/Obama-Yoda/Obama-Yoda-Inside.png" name="My Friend Obama"/>
+       <Card pic="/Minion-link/Minionlink-front.png" insidepic="/Minion-link/Minionlink-inside-preview.png" name="minimalist"/>
+       <Card pic="/Obama-Yoda/Obama-Yoda-Preview.png"  insidepic="/Obama-Yoda/Obama-Yoda-Inside.png" name="My Friend Obama"/>
+       <Card pic="/Minion-link/Minionlink-front.png" insidepic="/Minion-link/Minionlink-inside-preview.png" name="minimalist"/>
+       <Card pic="/Obama-Yoda/Obama-Yoda-Preview.png"  insidepic="/Obama-Yoda/Obama-Yoda-Inside.png" name="My Friend Obama"/>
+    </div>
+
+
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <div className="container">
         <p 
